a65119cbfdcc097d7536e16e28d436e67b5ab04c
Adam Austin <ataustin@gmail.com>
2020-03-28 22:41:12 -0500

    improve table and row selection for species sites

--- R/get-site-url-data.R
+++ R/get-site-url-data.R
@@ -7,9 +7,10 @@ get_site_url_data <- function(fact_sheet_url_data, species = "squirrel") {
   
   site_data <- dplyr::mutate(tmp,
                              site_html      = purrr::map(site_url, get_site_html),
-                             site_table     = purrr::map(site_html, get_site_table),
                              site_html_char = purrr::map_chr(site_html, as.character),
-                             is_species     = grepl(species, site_html_char, ignore.case = TRUE),
+                             is_species     = purrr::map_lgl(site_html_char, grep_species, species = species),
+                             site_tables    = purrr::map(site_html, get_site_tables),
+                             site_table     = purrr::map(site_tables, select_table),
                              species_row    = purrr::map(site_table, reduce_table, species = species))
 
   site_data
@@ -41,7 +42,7 @@ clean_region_names <- function(region_names) {
 }
 
 
-get_site_html <- function(site_url, sleep_interval = 0.3) {
+get_site_html <- function(site_url, sleep_interval = 0.5) {
   Sys.sleep(sleep_interval)
   site_html   <- xml2::read_html(site_url)
 
@@ -49,16 +50,26 @@ get_site_html <- function(site_url, sleep_interval = 0.3) {
 }
 
 
-get_site_table <- function(site_html) {
-  site_table <- tryCatch(rvest::html_table(site_html, header = TRUE, fill = TRUE),
-                         error = function(e) NULL)
-  table_checked <- if(check_table(site_table)) site_table[[1]] else NULL
-  
-  table_checked
+grep_species <- function(html_char, species) {
+  html_mentions_species    <- grepl(species, html_char, ignore.case = TRUE)
+  needs_special_adjustment <- grepl("Matthiessen Deer/Turkey Map", html_char, fixed = TRUE)
+
+  is_species <- html_mentions_species & !needs_special_adjustment
+
+  is_species
 }
 
 
-check_table <- function(table) {
+get_site_tables <- function(site_html) {
+  site_tables <- tryCatch(rvest::html_table(site_html, header = TRUE, fill = TRUE, trim = TRUE),
+                          error = function(e) NULL)
+  tables_checked <- if(check_tables(site_tables)) site_tables else NULL
+  
+  tables_checked
+}
+
+
+check_tables <- function(table) {
   if(!length(table)) return(FALSE)
 
   names_are_blank <- all(names(table) == "")
@@ -70,12 +81,38 @@ check_table <- function(table) {
 }
 
 
+select_table <- function(tables) {
+  # in the case in which there are multiple tables on a given location's page,
+  # the table with more rows contains a column indicating species.
+  if(length(tables) > 1) {
+    row_counts <- vapply(tables, nrow, integer(1))
+    max_row_ix <- which.max(row_counts)
+    table      <- tables[[max_row_ix]]
+  } else {
+    table <- tables[[1]]
+  }
+
+  table
+}
+
+
 reduce_table <- function(table, species = "squirrel") {
   if(is.null(table)) return(NULL)
 
-  species_ix  <- apply(table, 1, function(row) any(grepl(species, row, ignore.case = TRUE)))
-  species_row <- table[species_ix, ]
-  row <- if(length(species_row) == 0) NULL else species_row
+  col_is_species <- grepl("species|program", names(table), ignore.case = TRUE)
+  if(!any(col_is_species)) return(NULL)
 
+  species_col <- table[, col_is_species]
+  species_ix  <- which(grepl(species, species_col, ignore.case = TRUE))
+  species_row <- table[species_ix, ]
+  reduced_row <- if(nrow(species_row) == 0) NULL else clean_row(species_row, col_is_species)
+  cleaned_row <- 
+
+  reduced_row
+}
+
+
+clean_row <- function(row, which_col) {
+  row[, which_col] <- gsub("1.*", "", row[, which_col])
   row
 }
